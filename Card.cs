5c6145058127e9478a922618697bc83c1aafc385
kalebhings <kalebhings@gmail.com>
2022-05-17 11:22:01 -0600

    Added code to display card values and check guess

--- Card.cs
+++ Card.cs
@@ -33,5 +33,32 @@ namespace cse210_02
                 secondCard = random2.Next(1,14);
             }
         }
+        public void DisplayFirstCard()
+        {
+            Console.WriteLine($"The card is: {firstCard}");
+        }
+        public void DisplaySecondCard()
+        {
+            Console.WriteLine($"Next card was: {secondCard}");
+        }
+
+        public bool Guess(string guess)
+        {
+            bool goodGuess = false;
+            
+            if (firstCard < secondCard && guess == "h")
+            {
+                goodGuess = true;
+            }
+            else if (firstCard > secondCard && guess == "l")
+            {
+                goodGuess = true;
+            }
+            else
+            {
+                goodGuess = false;
+            }
+            return goodGuess;
+        }
     }
 }
